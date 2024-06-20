7515fbf93e4b4419ba9b1ce5ccfb27605350ffda
joaomgsb <joaomgsb@gmail.com>
2024-06-19 23:21:08 -0300

    Deletando assets

--- assets/script/fetch.js
+++
@@ -1,3 +0,0 @@
-export const fetchHeaders = {
-    'Content-Type': 'application/json',
-}
--- assets/script/routes.js
+++
@@ -1,4 +0,0 @@
-export default {
-    videos: "http://localhost:3001/videos",
-    comments: "http://localhost:3001/comments"
-}
