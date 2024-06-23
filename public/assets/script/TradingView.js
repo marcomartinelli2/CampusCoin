6ace6af0e633d88fbbd8584cf317c6cc7fc6c88c
joaomgsb <joaomgsb@gmail.com>
2024-06-23 20:38:05 -0300

    TradingView

---
+++ public/assets/script/TradingView.js
@@ -0,0 +1,14 @@
+new TradingView.widget({
+    "width": "100%",
+    "height": "100%",
+    "symbol": "BINANCE:BTCUSDT",
+    "interval": "D",
+    "timezone": "Etc/UTC",
+    "theme": "dark",
+    "style": "1",
+    "locale": "en",
+    "toolbar_bg": "#f1f3f6",
+    "enable_publishing": false,
+    "allow_symbol_change": true,
+    "container_id": "tradingview-widget-container"
+});
